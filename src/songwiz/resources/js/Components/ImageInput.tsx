6cb0ab2526ac015547e24b3ab0eb0613f29446d5
spectrespectre0 <williamadt123@gmail.com>
2024-11-25 14:24:20 +0700

    style: Refactor w/ prettier

--- src/songwiz/resources/js/Components/ImageInput.tsx
+++ src/songwiz/resources/js/Components/ImageInput.tsx
@@ -1,50 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function CustomFileInput() {
-    const [fileName, setFileName] = useState('');
-    const [previewUrl, setPreviewUrl] = useState('');
+    const [fileName, setFileName] = useState("");
+    const [previewUrl, setPreviewUrl] = useState("");
 
     // Handle file change
     const handleFileChange = (event) => {
         const file = event.target.files[0];
         if (file) {
             setFileName(file.name); // Update the file name display
-            if (file.type.startsWith('image/')) {
+            if (file.type.startsWith("image/")) {
                 const url = URL.createObjectURL(file);
                 setPreviewUrl(url);
-            }
-            else{
-                setPreviewUrl('null');
+            } else {
+                setPreviewUrl("null");
             }
         }
     };
 
     // Trigger file input click when custom button is clicked
     const handleButtonClick = () => {
-        document.getElementById('hidden-file-input').click();
+        document.getElementById("hidden-file-input").click();
     };
 
     return (
         <div className="">
-            {previewUrl &&
+            {previewUrl && (
                 <div className="flex justify-center items-center">
-                    <img src={previewUrl} alt="Preview" className="w-sm"/>
+                    <img src={previewUrl} alt="Preview" className="w-sm" />
                 </div>
-            }
+            )}
             {/* Hidden file input */}
             <input
                 id="hidden-file-input"
                 type="file"
                 accept="image/*"
                 onChange={handleFileChange}
-                style={{ display: 'none' }} // Hide the default file input
+                style={{ display: "none" }} // Hide the default file input
             />
             {/* Display selected file name */}
             {fileName && <p>Selected File: {fileName}</p>}
             {/* Custom button to trigger file input */}
-            <button onClick={handleButtonClick}
-                    className="text-3xl px-5 border-1 rounded-md text-black/70 hover:scale-150 transition duration-200 "
-
+            <button
+                onClick={handleButtonClick}
+                className="text-3xl px-5 border-1 rounded-md text-black/70 hover:scale-150 transition duration-200 "
             >
                 Upload File
             </button>
--- src/songwiz/resources/js/Pages/Welcome.tsx
+++ src/songwiz/resources/js/Pages/Welcome.tsx
@@ -1,7 +1,7 @@
-import { PageProps } from '@/types';
-import { Head, Link } from '@inertiajs/react';
+import { PageProps } from "@/types";
+import { Head, Link } from "@inertiajs/react";
 import ImageInput from "@/Components/ImageInput";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Welcome({
     auth,
@@ -10,13 +10,13 @@ export default function Welcome({
 }: PageProps<{ laravelVersion: string; phpVersion: string }>) {
     const handleImageError = () => {
         document
-            .getElementById('screenshot-container')
-            ?.classList.add('!hidden');
-        document.getElementById('docs-card')?.classList.add('!row-span-1');
+            .getElementById("screenshot-container")
+            ?.classList.add("!hidden");
+        document.getElementById("docs-card")?.classList.add("!row-span-1");
         document
-            .getElementById('docs-card-content')
-            ?.classList.add('!flex-row');
-        document.getElementById('background')?.classList.add('!hidden');
+            .getElementById("docs-card-content")
+            ?.classList.add("!flex-row");
+        document.getElementById("background")?.classList.add("!hidden");
     };
 
     return (
@@ -27,12 +27,16 @@ export default function Welcome({
                     <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                         <header className="grid grid-cols-2 items-center gap-2  lg:grid-cols-3">
                             <div className="flex lg:col-start-2 lg:justify-center">
-                                <img alt="Logo" src="/images/logo.png" className="w-4/5 "/>
+                                <img
+                                    alt="Logo"
+                                    src="/images/logo.png"
+                                    className="w-4/5 "
+                                />
                             </div>
                             <nav className="-mx-3 mt-3 flex flex-1 justify-end">
                                 {auth.user ? (
                                     <Link
-                                        href={route('dashboard')}
+                                        href={route("dashboard")}
                                         className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
                                     >
                                         Dashboard
@@ -40,13 +44,13 @@ export default function Welcome({
                                 ) : (
                                     <>
                                         <Link
-                                            href={route('login')}
+                                            href={route("login")}
                                             className="text-xl font-bold rounded-md px-3 py-2 text-black/70 ring-1 ring-transparent transition hover:text-red-500 hover:scale-110 focus:outline-none focus-visible:ring-[#FF2D20]"
                                         >
                                             Login
                                         </Link>
                                         <Link
-                                            href={route('register')}
+                                            href={route("register")}
                                             className="text-xl font-bold rounded-md px-3 py-2 text-black/70 ring-1 ring-transparent transition hover:text-red-500 hover:scale-110 focus:outline-none focus-visible:ring-[#FF2D20]"
                                         >
                                             Register
@@ -60,14 +64,12 @@ export default function Welcome({
                             <div className="w-3/4 mx-auto">
                                 <div className="flex flex-col items-center justify-center">
                                     <div className="flex flex-row w-full">
-                                        <div
-                                            className="items-center justify-center flex flex-1 rounded-t-lg py-3 border-2 border-gray-50 bg-gray-50/50 active:scale-95">
+                                        <div className="items-center justify-center flex flex-1 rounded-t-lg py-3 border-2 border-gray-50 bg-gray-50/50 active:scale-95">
                                             <button className="text-center">
                                                 Audio
                                             </button>
                                         </div>
-                                        <div
-                                            className="items-center justify-center flex flex-1 rounded-t-lg py-3 border-2 border-gray-50 bg-gray-50/50 active:scale-95">
+                                        <div className="items-center justify-center flex flex-1 rounded-t-lg py-3 border-2 border-gray-50 bg-gray-50/50 active:scale-95">
                                             <button className="text-center">
                                                 Image
                                             </button>
@@ -78,11 +80,21 @@ export default function Welcome({
                                     </div>
                                 </div>
                             </div>
-
                         </main>
 
                         <footer className="py-16 text-center text-sm text-black /70">
-                            <p>Made by <span className="text-base font-bold underline hover:text-blue-700"><a href="https://github.com/kirisame-ame" target="_blank" rel="noopener noreferrer">kirisame-ame</a></span></p>
+                            <p>
+                                Made by{" "}
+                                <span className="text-base font-bold underline hover:text-blue-700">
+                                    <a
+                                        href="https://github.com/kirisame-ame"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        kirisame-ame
+                                    </a>
+                                </span>
+                            </p>
                         </footer>
                     </div>
                 </div>
