b1c67e09edb14c76c9129de679ce796110b9cb8c
William Andrian Dharma T <williamadt123@gmail.com>
2024-12-19 22:22:48 +0700

    server timeout change

--- src/songwiz-api/gunicorn/gunicorn_config.py
+++ src/songwiz-api/gunicorn/gunicorn_config.py
@@ -6,7 +6,7 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
-# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+timeout = int(os.environ.get('GUNICORN_TIMEOUT', '1000'))
 
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5000')
 
