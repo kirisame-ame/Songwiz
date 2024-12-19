51ef6aced4512e9eafce227b32cbbeddc7b5dd10
William Andrian Dharma T <williamadt123@gmail.com>
2024-12-19 23:42:39 +0700

    Update gunicorn_config.py

--- src/songwiz-api/gunicorn/gunicorn_config.py
+++ src/songwiz-api/gunicorn/gunicorn_config.py
@@ -6,7 +6,7 @@ workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 
 threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
-timeout = int(os.environ.get('GUNICORN_TIMEOUT', '1000'))
+timeout = 600
 
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5000')
 
