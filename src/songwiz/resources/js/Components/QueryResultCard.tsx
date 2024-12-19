18d7e2433646fb2770ea8a6fb10c17f7952f8002
William Andrian Dharma T <williamadt123@gmail.com>
2024-12-20 03:24:32 +0700

    query image bug fix

--- src/songwiz/resources/js/Components/QueryResultCard.tsx
+++ src/songwiz/resources/js/Components/QueryResultCard.tsx
@@ -90,7 +90,7 @@ const QueryResultCard: React.FC<QueryResultCardProps> = ({ trackData }) => {
                                 {index + 1}
                             </p>
                             <img
-                                src={API_URL + 'fetch/img/' + track.cover_path}
+                                src={API_URL + '/fetch/img/' + track.cover_path}
                                 alt={track.name}
                                 className="h-48 w-48 rounded-full object-cover"
                             />
