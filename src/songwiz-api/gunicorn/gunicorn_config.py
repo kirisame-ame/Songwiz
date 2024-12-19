b521ed668e6a1b75e5ecaa54edb0ac6c9ce7a63a
William Andrian Dharma T <williamadt123@gmail.com>
2024-12-20 00:27:02 +0700

    Update gunicorn_config.py

--- src/songwiz-api/gunicorn/gunicorn_config.py
+++ src/songwiz-api/gunicorn/gunicorn_config.py
@@ -13,3 +13,7 @@ bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:5000')
 forwarded_allow_ips = '*'
 
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+accesslog = '-'
+errorlog = '-'
+loglevel = 'debug'
