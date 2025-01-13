2a66dcc9663774328665c5dcf3015fc917fb2225
William Andrian Dharma T <williamadt123@gmail.com>
2025-01-13 16:26:48 +0700

    feat: adds timestamp to mapper download

--- src/songwiz/resources/js/Components/MapperUpload.tsx
+++ src/songwiz/resources/js/Components/MapperUpload.tsx
@@ -44,7 +44,9 @@ function MapperUpload() {
             const url = window.URL.createObjectURL(new Blob([response.data]))
             const link = document.createElement('a')
             link.href = url
-            link.setAttribute('download', 'mapper.json')
+            const date = new Date()
+            const formattedDate = date.toISOString().split('T')[0]
+            link.setAttribute('download', `mapper_${formattedDate}.json`)
             document.body.appendChild(link)
             link.click()
         } catch (err) {
